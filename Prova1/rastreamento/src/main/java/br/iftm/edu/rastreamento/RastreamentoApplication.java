a9c5f411d39f8350ea5a4354384052e37c495431
Lucca <136863244+LukkzHS@users.noreply.github.com>
2024-08-01 19:39:02 -0300

    Criacao do projeto da prova

---
+++ Prova1/rastreamento/src/main/java/br/iftm/edu/rastreamento/RastreamentoApplication.java
@@ -0,0 +1,13 @@
+package br.iftm.edu.rastreamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastreamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastreamentoApplication.class, args);
+	}
+
+}
---
+++ Prova1/rastreamento/src/test/java/br/iftm/edu/rastreamento/RastreamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.iftm.edu.rastreamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RastreamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
